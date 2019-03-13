28553247477707312c14939cfad283b7c887da49
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-13 10:13:34 +0100

    Add vueslider and graph

--- src/main.js
+++ src/main.js
@@ -3,17 +3,25 @@ import VueX from 'vuex'
 import router from './router'
 import App from './App.vue'
 import Buefy from 'buefy'
-import fr from 'vee-validate/dist/locale/fr';
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+import fr from 'vee-validate/dist/locale/fr'
 import VeeValidate, { Validator } from 'vee-validate'
 import 'buefy/dist/buefy.css'
+import VueGraph from 'vue-graph'
+import GraphBar from 'vue-graph/src/components/bar'
 
 Vue.use(VueX)
 Vue.use(Buefy)
 Vue.use(VeeValidate)
+Vue.use(VueGraph)
 
 Validator.localize('fr', fr)
 
 Vue.config.productionTip = false
+
+Vue.component(GraphBar.name, GraphBar);
+Vue.component('VueSlider', VueSlider)
 
 new Vue({
   router,
