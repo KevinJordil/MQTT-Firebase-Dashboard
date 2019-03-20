f9fa38e5ba6262b299a9ba9c81f21aa77973c917
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-20 10:22:06 +0100

    Add id

--- src/components/navbar/Logged.vue
+++ src/components/navbar/Logged.vue
@@ -17,10 +17,10 @@
           <b-dropdown aria-role="list" position="is-bottom-left">
             <a class="navbar-item" slot="trigger" role="button">
               <img :src="user.photoURL">
-              <span>  {{ user.displayName }}</span>
+              {{ user.displayName }}
               <b-icon icon="menu-down"></b-icon>
             </a>
-
+            <b-dropdown-item aria-role="listitem" @click="ShowId">Mon id</b-dropdown-item>
             <b-dropdown-item aria-role="listitem" @click="SignOut">Deconnexion</b-dropdown-item>
           </b-dropdown>
         </div>
@@ -40,11 +40,22 @@ export default {
     };
   },
   methods: {
+    ShowId() {
+      this.$dialog.alert({
+        title: "Mon Id",
+        message: `Mon identifiant est : <strong>${this.user.uid}</strong>`,
+        confirmText: "OK, j'ai noté !"
+      });
+    },
     SignOut() {
-      fire
-        .auth()
-        .signOut()
-        .then(() => {});
+      try {
+        fire
+          .auth()
+          .signOut()
+          .then(() => {});
+      } catch (e) {
+        console.log(e);
+      }
     }
   },
   mounted: async function() {
